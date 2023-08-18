0f03bf9e32f4c03f0f1127d7e15d8deb913eb1b5
DHK2001 <dhenrygk12@gmail.com>
2023-08-18 09:34:59 -0600

    correccion visual

--- lib/Drivers/Screens/Chat/component/audio.dart
+++ lib/Drivers/Screens/Chat/component/audio.dart
@@ -70,7 +70,7 @@ class _AudioContainerState extends State<AudioContainer> {
                     });
                   },
                   icon: !audioPlaying
-                      ? Icon(Icons.play_arrow, color: colorIcono)
+                      ? Icon(Icons.play_arrow, color: Colors.white)
                       : Icon(Icons.stop, color: Colors.red),
                 ),
           if (audioDuration != null)
@@ -78,15 +78,15 @@ class _AudioContainerState extends State<AudioContainer> {
               children: [
                 Text(
                   '${audioPosition?.inMinutes ?? 0}:${(audioPosition?.inSeconds ?? 0).toString().padLeft(2, '0')}',
-                  style: TextStyle(fontSize: 10, color: colorIcono),
+                  style: TextStyle(fontSize: 10, color: Colors.white),
                 ),
                 Text(
                   ' / ',
-                  style: TextStyle(fontSize: 10, color: colorIcono),
+                  style: TextStyle(fontSize: 10, color: Colors.white),
                 ),
                 Text(
                   '${audioDuration!.inMinutes}:${(audioDuration!.inSeconds % 60).toString().padLeft(2, '0')}',
-                  style: TextStyle(fontSize: 10, color: colorIcono),
+                  style: TextStyle(fontSize: 10, color: Colors.white),
                 ),
                 Icon(
                   Icons.done,
